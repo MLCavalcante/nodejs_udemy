aae8e3260338d3b9e72702c7c8d3c0f9030831bd
MLCavalcante <mariacova7@outlook.com>
2023-05-06 18:13:23 -0300

    initial commit

---
+++ routes/jobs.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const router  = express.Router()
+const Job     = require('../models/Job')
+
+//rota teste
+router.get('/test', (req, res) => {
+    res.send('deu certo')
+})
+
+// detalhe da vaga 
+ router.get('/view/:id', (req, res) => Job.findOne({
+    where:{id: req.params.id}
+ }).then(job => {
+  
+    res.render('view',{
+        job
+    })
+
+ }).catch(err => console.log(err))
+ )
+
+
+//form de rota de envio 
+router.get('/add', (req, res) => {
+    res.render('add')
+})
+
+//add job via post
+router.post('/add', (req, res) => {
+   let {title, description, salary, company, email, new_job} = req.body
+
+   // insert dados no sistema 
+   Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job
+})
+.then(() => res.redirect('/'))
+.catch(err => console.log(err))
+
+
+})
+
+module.exports = router
